4162c4ef6215c1f9f3713809de024f7cdf8328f3
Luis Lavena <luislavena@gmail.com>
2011-09-07 08:18:38 -0300

    Bench really long paths (315 characters)

--- bench/file_expand_path.rb
+++ bench/file_expand_path.rb
@@ -1,6 +1,13 @@
 require "benchmark"
 require "fenix"
 
+# build a huge path for testing (319 characters long)
+names = ["abc", "def", "mno", "xyz"]
+tmp = []
+80.times { tmp << names.sample }
+long_path = tmp.join(File::SEPARATOR)
+full_long_path = File.join("C:", long_path)
+
 TESTS = 10_000
 
 puts "File.expand_path: #{TESTS} times."
@@ -28,4 +35,16 @@ Benchmark.bmbm do |results|
 
   results.report("Ruby '~', 'C:/Foo'") { TESTS.times {  File.expand_path('~', 'C:/Foo') } }
   results.report("Fenix '~', 'C:/Foo'") { TESTS.times { Fenix::File.expand_path('~', 'C:/Foo') } }
+
+  results.report("Ruby long_path") { TESTS.times { File.expand_path(long_path) } }
+  results.report("Fenix long_path") { TESTS.times { Fenix::File.expand_path(long_path) } }
+
+  results.report("Ruby long_path, 'rel'") { TESTS.times { File.expand_path(long_path, 'rel') } }
+  results.report("Fenix long_path, 'rel'") { TESTS.times { Fenix::File.expand_path(long_path, 'rel') } }
+
+  results.report("Ruby long_path, 'C:/Foo'") { TESTS.times { File.expand_path(long_path, 'C:/Foo') } }
+  results.report("Fenix long_path, 'C:/Foo'") { TESTS.times { Fenix::File.expand_path(long_path, 'C:/Foo') } }
+
+  results.report("Ruby full_long_path") { TESTS.times { File.expand_path(full_long_path) } }
+  results.report("Fenix full_long_path") { TESTS.times { Fenix::File.expand_path(full_long_path) } }
 end
