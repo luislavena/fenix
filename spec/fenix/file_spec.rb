3be82feabdf678d97605f337d4da70ba79d06547
Luis Lavena <luislavena@gmail.com>
2011-09-07 15:39:22 -0300

    File.expand_path spec completeness

--- spec/fenix/file_spec.rb
+++ spec/fenix/file_spec.rb
@@ -64,12 +64,36 @@ describe Fenix::File do
         subject.expand_path("~", "C:/FooBar").must_equal home
         subject.expand_path("~/a", "C:/FooBar").must_equal File.join(home, "a")
       end
+
+      it "does not modify a HOME string argument" do
+        str = "~/a"
+        subject.expand_path(str).must_equal "#{home}/a"
+        str.must_equal "~/a"
+      end
     end
 
     it "raises a TypeError if not passed a String type" do
       proc { subject.expand_path(1)    }.must_raise TypeError
       proc { subject.expand_path(nil)  }.must_raise TypeError
       proc { subject.expand_path(true) }.must_raise TypeError
+    end
+
+    it "expands C:/./dir to C:/dir" do
+      subject.expand_path("C:/./dir").must_equal "C:/dir"
+    end
+
+    it "does not modify the string argument" do
+      str = "./a/b/../c"
+      subject.expand_path(str, base).must_equal "#{base}/a/c"
+      str.must_equal "./a/b/../c"
+    end
+
+    it "returns a String when passed a String subclass" do
+      sub = Class.new(String)
+      str = sub.new "./a/b/../c"
+      path = subject.expand_path(str, base)
+      path.must_equal "#{base}/a/c"
+      path.must_be_instance_of(String)
     end
   end
 end
